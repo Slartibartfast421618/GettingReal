d2ef00aea11052465292a71b70546ed16ee5a7ff
MHLa <mhla63314@edu.ucl.dk>
2024-05-09 12:22:15 +0200

    Base files along with .gitignore creation

---
+++ GettingReal-Jacobsens_Bakery/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GettingReal_Jacobsens_Bakery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
