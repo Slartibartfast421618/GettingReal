42d4932b0f63be1b8a73ac5b158441ee7db5c25d
MHLa <mhla63314@edu.ucl.dk>
2024-05-15 11:00:20 +0200

    UnitTest TestProduction debugged

--- GettingReal-Jacobsens_Bakery/Model/Production.cs
+++ GettingReal-Jacobsens_Bakery/Model/Production.cs
@@ -14,14 +14,14 @@ namespace GettingReal_Jacobsens_Bakery.Model
         public DateTime ProdStart
         {
             get { return _prodStart; }
-            set { _prodStart = DateTime.Now; }
+            set { _prodStart = value; }
         }
         private DateTime _prodEnd;
 
         public DateTime ProdEnd
         {
             get { return _prodEnd; }
-            set { _prodEnd = DateTime.Now; }
+            set { _prodEnd = value; }
         }
         private string _prodOrderId;
 
